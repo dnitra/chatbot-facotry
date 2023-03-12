766e93d953fa8222434e84cee6e8f55f012e7fa1
dnitra <109307365+dnitra@users.noreply.github.com>
2023-03-12 16:33:48 +0100

    adjusted title

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -8,7 +8,7 @@ import { RouteContext } from '@/Hooks/useRoute';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 const appName =
-  window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
+  window.document.getElementsByTagName('title')[0]?.innerText || 'Chatbot factory';
 
 createInertiaApp({
   title: title => `${title} - ${appName}`,
